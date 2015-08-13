d6c379b1fd40ead3914f5ee55b137b03deec1406
pyar6329 <pyar6329@gmail.com>
2015-08-13 21:26:08 +0900

    fix post codeclimate coverage

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -93,5 +93,6 @@ RSpec.configure do |config|
 =end
   config.color = true
   # CodeClimate::TestReporter.start unless ENV['CODECLIMATE_REPO_TOKEN'].nil?
-  CodeClimate::TestReporter.start if ENV['CODECLIMATE_REPO_TOKEN'].present? && ENV['TRAVIS_BRANCH'] == "master"
+  CodeClimate::TestReporter.start if ENV['CODECLIMATE_REPO_TOKEN'].present? && ENV['TRAVIS_BRANCH'] == "master" &&
+      ENV['TRAVIS_PULL_REQUEST'] == false
 end
