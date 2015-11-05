d399634058e2392e9699914ed00a11eda3d5fac0
pyar6329 <pyar6329@gmail.com>
2015-11-05 21:10:04 +0900

    fix devise on rspec

--- spec/controllers/users_controller_spec.rb
+++ spec/controllers/users_controller_spec.rb
@@ -2,6 +2,25 @@ require 'rails_helper'
 
 RSpec.describe UsersController, type: :controller do
 
+  # as test by user
+  login_user
+
+  # This should return the minimal set of attributes required to create a valid
+  # User. As you add validations to User, be sure to
+  # adjust the attributes here as well.
+  let(:valid_attributes) {
+    skip("Add a hash of attributes valid for your model")
+  }
+
+  let(:invalid_attributes) {
+    skip("Add a hash of attributes invalid for your model")
+  }
+
+  # This should return the minimal set of values that should be in the session
+  # in order to pass any filters (e.g. authentication) defined in
+  # UsersController. Be sure to keep this updated too.
+  let(:valid_session) { {} }
+
   describe "GET #index" do
     it "returns http success" do
       get :index
@@ -10,9 +29,10 @@ RSpec.describe UsersController, type: :controller do
   end
 
   describe "GET #show" do
-    it "returns http success" do
-      get :show
-      expect(response).to have_http_status(:success)
+    it "assigns the requested user as @user" do
+      user = User.create! valid_attributes
+      get :show, {:id => user.to_param}, valid_session
+      expect(assigns(:user)).to eq(user)
     end
   end
 
--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,6 +1,18 @@
 FactoryGirl.define do
-  factory :user do
-    
+  factory :admin, class: User do
+    id 60000
+    email "testadmin@test.com"
+    nickname "testadmin"
+    password "foobartest"
+    password_confirmation "foobartest"
+  end
+
+  factory :user, class: User do
+    id 60001
+    email "testuser@test.com"
+    nickname "testuser"
+    password "foobartest"
+    password_confirmation "foobartest"
   end
 
 end
